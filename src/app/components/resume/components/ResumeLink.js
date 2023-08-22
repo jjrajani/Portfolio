f4c44560083bd7a279b744b4e3d23f9530baa5d2
Jenna Rajani <jjrajani@gmail.com>
2023-08-22 16:08:16 -0400

    update resume link

--- src/app/components/resume/components/ResumeLink.js
+++ src/app/components/resume/components/ResumeLink.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const ResumeLink = () => {
     const resumeLink =
-        'https://docs.google.com/document/d/1yOKCl-K9FTa4ZWGj-j4S-yJ5dAlAgG4H0Qpb6lrJFcg/edit?usp=sharing';
+        'https://docs.google.com/document/d/e/2PACX-1vSICb07zLnnm3vg0O26rKDil8BNFAUFO_QaENf3b9RUbkQHfgLwGmKhlR3pWSFsbWJ7tIsxkwnbn_WC/pub';
     return (
         <div className="resume_download">
             <a href={resumeLink} target="blank">
-                <i className="fa fa-file-pdf-o" aria-hidden="true" />Dowload PDF
+                <i className="fa fa-file-pdf-o" aria-hidden="true" />View Resume Doc
             </a>
         </div>
     );
